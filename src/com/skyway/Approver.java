0f0d3d233643d6a4b8f7015993e6192d884d5346
DESKTOP-TRO4L8E\Admin <a.pagoda@unitsky.com>
2024-03-14 16:51:44 +0300

    update Approver. владелец маршрута, исполнитель задачи

--- src/com/skyway/Approver.java
+++ src/com/skyway/Approver.java
@@ -55,32 +55,48 @@ public class Approver extends SkyService {
                 } else {
                     task.put("finish", String.valueOf(Calendar.getInstance().getTime()));
                 }
+                Map<String, String> route_owner = findObject(ctx, "Inbox Task", task.get("name"),//владелец маршрута
+                        "from[Route Task].to.owner:caowner");
+                Map<String, String> caowner = findObject(ctx, "Person", route_owner.get("caowner"),
+                        "attribute[First Name]:firstname",
+                        "attribute[Last Name]:lastname");
+                task.put("caowner", caowner.get("firstname").substring(0, 1) + ". " + caowner.get("lastname") + " (" + route_owner.get("caowner") + ")");
+                Map<String, String> assigner = new HashMap<>();//                                    //исполнитель задачи
                 String caId = scalar(ctx, task.get("id"), "from[Route Task].to.to[Object Route].to.to[Object Route].from.id");
                 if (caId != null) {
                     Map<String, String> ca = row(ctx, caId,
                             "name:caname",
-                            "owner:caowner",
+//                            "owner:caowner",
                             "current"/*,
                             "description:cadescription"*/);
 
                     if (ca.get("current").equals("In Approval"))
                         if (task.get("assigner_type").equals("Person")) {
-                            if (task.get("assigner_name").equals(username))
+                            if (task.get("assigner_name").equals(username)) {
+                                assigner = findObject(ctx, "Person", task.get("assigner_name"),
+                                        "attribute[First Name]:firstname",
+                                        "attribute[Last Name]:lastname");
+                                task.put("assigner_name", assigner.get("firstname").substring(0, 1) + ". " + assigner.get("lastname") + " (" + task.get("assigner_name") + ")");
                                 my_tasks.add(task);
+                            }
                         } else {
                             group_tasks.add(task);
                         }
 
 
-                    Map<String, String> caowner = findObject(ctx, "Person", ca.get("caowner"),
-                            "attribute[First Name]:firstname",
-                            "attribute[Last Name]:lastname");
-
-                    ca.put("caowner", caowner.get("firstname").substring(0, 1) + ". " + caowner.get("lastname") + " (" + ca.get("caowner") + ")");
+//                    Map<String, String> caowner = findObject(ctx, "Person", ca.get("caowner"),
+//                            "attribute[First Name]:firstname",
+//                            "attribute[Last Name]:lastname");
+//
+//                    ca.put("caowner", caowner.get("firstname").substring(0, 1) + ". " + caowner.get("lastname") + " (" + ca.get("caowner") + ")");
 
                     task.putAll(ca);
                 } else {
                     if (task.get("assigner_type") != null && task.get("assigner_name") != null && task.get("assigner_type").equals("Person") && task.get("assigner_name").equals(username)) {
+                        assigner = findObject(ctx, "Person", task.get("assigner_name"),
+                                "attribute[First Name]:firstname",
+                                "attribute[Last Name]:lastname");
+                        task.put("assigner_name", assigner.get("firstname").substring(0, 1) + ". " + assigner.get("lastname") + " (" + task.get("assigner_name") + ")");
                         my_tasks.add(task);
                     }
                 }
@@ -160,10 +176,16 @@ public class Approver extends SkyService {
                 Map<String, String> first_route_task = route_tasks.get(0);
                 route.put("task", first_route_task.get("task"));
                 route.put("description", first_route_task.get("description"));
-                route.put("assigner", first_route_task.get("assigner_first_name") + " " + first_route_task.get("assigner_last_name"));
+                if (first_route_task.get("assigner_first_name") != null && first_route_task.get("assigner_last_name") != null && !first_route_task.get("assigner_first_name").equals("") && !first_route_task.get("assigner_last_name").equals("")) {
+                    route.put("assigner", first_route_task.get("assigner_first_name").substring(0, 1) + ". " + first_route_task.get("assigner_last_name") + " (" + first_route_task.get("assigner") + ")");
+                }
+                Map<String, String> route_owner = findObject(ctx, "Person", route.get("owner"),
+                        "attribute[First Name]:firstname",
+                        "attribute[Last Name]:lastname");
+                route.put("owner", route_owner.get("firstname").substring(0, 1) + ". " + route_owner.get("lastname") + " (" + route.get("owner") + ")");
                 route.put("assigned_time", printDateFormat.format(dateFormat.parse(first_route_task.get("assigned_time"))));
                 route.put("finish", printDateFormat.format(dateFormat.parse(first_route_task.get("finish"))));
-//                route.put("finish", first_route_task.get("finish"));
+
                 if (route.get("status") != null && route.get("status").equals("Stopped")) {
                     route.put("color", "#ffd9d9");
                 }
@@ -212,18 +234,27 @@ public class Approver extends SkyService {
                         "to[Route Task].from.name:task", "to[Route Task].modified:assigned_time",
                         "to[Route Task].from.attribute[Scheduled Completion Date]:finish",
                         "to[Route Task].from.attribute[Title]:description",
+                        "to[Route Task].from.from[Project Task].to:assigner",
                         "to[Route Task].from.from[Project Task].to.attribute[First Name]:assigner_first_name",
                         "to[Route Task].from.from[Project Task].to.attribute[Last Name]:assigner_last_name");
                 Map<String, String> first_route_task = route_tasks.get(0);
                 route.put("task", first_route_task.get("task"));
                 route.put("description", first_route_task.get("description"));
-                route.put("assigner", first_route_task.get("assigner_first_name") + " " + first_route_task.get("assigner_last_name"));
+                if (first_route_task.get("assigner_first_name") != null && first_route_task.get("assigner_last_name") != null && !first_route_task.get("assigner_first_name").equals("") && !first_route_task.get("assigner_last_name").equals("")) {
+                    route.put("assigner", first_route_task.get("assigner_first_name").substring(0, 1) + ". " + first_route_task.get("assigner_last_name") + " (" + first_route_task.get("assigner") + ")");
+                }
+                Map<String, String> caowner = findObject(ctx, "Person", route.get("owner"),
+                        "attribute[First Name]:firstname",
+                        "attribute[Last Name]:lastname");
+                route.put("owner", caowner.get("firstname").substring(0, 1) + ". " + caowner.get("lastname") + " (" + route.get("owner") + ")");
+
                 route.put("assigned_time", printDateFormat.format(dateFormat.parse(first_route_task.get("assigned_time"))));
                 if (first_route_task.get("finish") != null && !first_route_task.get("finish").equals("") ) {
                     route.put("finish", printDateFormat.format(dateFormat.parse(first_route_task.get("finish"))));
                 } else {
                     route.put("finish", String.valueOf(Calendar.getInstance().getTime()));
                 }
+
             }
 
             Map<String, Object> result = new LinkedHashMap<>();
