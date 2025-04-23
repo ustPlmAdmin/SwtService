f06ea5fc74c4a4f3dbdd53b06608d30413a5637b
DESKTOP-TRO4L8E\Admin <a.pagoda@unitsky.com>
2025-04-23 12:36:01 +0300

    update Monitor. иерархия маршрутов и задач

--- src/com/skyway/Monitor.java
+++ src/com/skyway/Monitor.java
@@ -12,19 +12,26 @@ import java.io.IOException;
 import java.util.*;
 
 public class Monitor extends SkyService {
-    List<Map<String, String>> history = new ArrayList<>();
     @GET
     @Path("/monitor_export")
     public Response getTaskHistory(@javax.ws.rs.core.Context HttpServletRequest request, @QueryParam("erp_name") String erpName) {
 
-        history.clear();
         Map<String, Object> result = new LinkedHashMap<>();
+        String type;
 
         try {
             Context ctx = authenticate(request);
-            List<Map<String, String>> children = getChild(ctx, erpName);
-            history = getInner(ctx, children, erpName);
-            result.put("history", history);
+            String taskId = findScalar(ctx, "*", erpName, "id");
+
+            if (erpName.startsWith("ERP"))  {
+                type = "Task";
+            } else if (erpName.startsWith("R")) {
+                type = "Route";
+            } else {
+                type = "Inbox Task";
+            }
+            result = deti(ctx, type, taskId, erpName);
+
         } catch (IOException | FrameworkException e) {
             e.printStackTrace();
         }
@@ -32,31 +39,67 @@ public class Monitor extends SkyService {
         return response(result);
     }
 
-    private List<Map<String, String>> getChild(Context ctx, String parent) throws FrameworkException {
-        List<Map<String, String>> child = new LinkedList<>();
-        if (parent.startsWith("E")) {
-            child = findRows(ctx, "Task", parent, "from[Object Route].to:name", "from[Object Route].to.attribute[Title]:title", "from[Object Route].to.current:status", "from[Object Route].to.id:id");
-        } else if (parent.startsWith("R")) {
-            child = findRows(ctx, "Route", parent, "to[Route Task].from:name","to[Route Task].from.attribute[Title]:title", "to[Route Task].from.current:status", "to[Route Task].from.id:id");
-        } else {
-            child = findRows(ctx, "Inbox Task", parent, "from[Task Sub Route].to:name", "from[Task Sub Route].to.attribute[Title]:title", "from[Task Sub Route].to.current:status", "from[Task Sub Route].to.id:id");
+    private Map<String, Object> deti(Context ctx, String type, String id, String name) throws FrameworkException {
+        List<Map<String, Object>> children = new LinkedList<>();
+        Map<String, Object> realChildren = new LinkedHashMap<>();
+        Map<String, Object> child = new LinkedHashMap<>();
+        child.put("name", name);
+        child.put("title", findScalar(ctx, type, name, "attribute[Title]"));
+        child.put("status", findScalar(ctx, type, name, "current"));
+        String conn = "";
+        switch (type) {
+            case "Task":
+                conn = "from[Object Route].to";
+                realChildren = tree(ctx, id, conn);
+                type = "Route";
+                break;
+            case "Route":
+                conn = "to[Route Task].from";
+                realChildren = tree(ctx, id, conn);
+                type = "Inbox Task";
+                break;
+            case "Inbox Task":
+                conn = "from[Task Sub Route].to";
+                realChildren = tree(ctx, id, conn);
+                type = "Route";
+                break;
         }
+        for (Object c : realChildren.values()) {
+            ArrayList<String> list = new ArrayList<>();
+            if (c.getClass().getName().equals("java.lang.String")) {
+                if (isValid(String.valueOf(c), type)) {
+                    Map<String, Object> deti = deti(ctx, type, findScalar(ctx, type, String.valueOf(c), "id"), String.valueOf(c));
+                    if (!deti.isEmpty()) {
+                        child.put("children", deti);
+                    } else {
+                        child.put("children", realChildren.get(conn));
+                    }
+                }
+            } else {
+                list = (ArrayList<String>) c;
+                for (String str : list) {
+                    if (isValid(str, type)) {
+                        children.add(deti(ctx, type, findScalar(ctx, type, str, "id"), str));
+                    }
+                }
+                child.put("children", children);
+            }
+        }
+
         return child;
     }
 
-    private List<Map<String, String>> getInner(Context ctx, List<Map<String, String>> children,  String p) throws FrameworkException {
-//        List<Map<String, String>> history = new ArrayList<>();
-
-        if (children.size() > 0) {
-            for (Map<String, String> c : children) {
-                history.add(c);
-                children = getChild(ctx, c.get("name"));
-                getInner(ctx, children, c.get("name"));
-            }
-
-
+    boolean isValid (String name, String type) {
+        if (type.equals("Task") && name.startsWith("ERP")) {
+            return true;
+        } else if (type.equals("Route") && name.startsWith("R-")) {
+            return true;
+        } else if (type.equals("Inbox Task") && name.startsWith("IT-")) {
+            return true;
+        } else {
+            return false;
         }
-            return history;
+
     }
 
 }
